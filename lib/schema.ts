462b859b33662fc8d0ec57789b2f51056d30cf0d
Juan Villacorta <jvillacorta@norkut.com.ve>
2021-11-01 17:28:41 -0400

    __dirname

--- lib/schema.ts
+++ lib/schema.ts
@@ -2,17 +2,19 @@ import { makeSchema } from 'nexus'
 import * as types from '../types'
 
 import { fileURLToPath } from 'url'
-import path from 'path'
+import path, { dirname } from 'path'
+
+const __dirname = fileURLToPath(dirname(import.meta.url));
 
 export const schema = makeSchema({
   types,
   outputs: {
-    schema: path.join(fileURLToPath(import.meta.url), '../../schema.graphql'),
-    typegen: path.join(fileURLToPath(import.meta.url), '../generated/nexus.d.ts'),
+    schema: path.join(__dirname, '../schema.graphql'),
+    typegen: path.join(__dirname, 'generated/nexus.d.ts'),
   },
   contextType: {
     export: 'Context',
-    module: path.join(fileURLToPath(import.meta.url), '../context.ts'),
+    module: path.join(__dirname, 'context.ts'),
   },
   sourceTypes: {
     modules: [
